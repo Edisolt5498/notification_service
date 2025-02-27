1176c759ccfcc757a5ea448911871babfd9ba78e
narol01 <kmingogod@gmail.com>
2025-02-27 12:29:21 +0100

    telegram-bot-final-BJS2-54566-narol merge and solved conflict with dev branches

--- src/main/java/faang/school/notificationservice/model/enums/PreferredContact.java
+++ src/main/java/faang/school/notificationservice/model/enums/PreferredContact.java
@@ -4,13 +4,4 @@ public enum PreferredContact {
     EMAIL,
     SMS,
     TELEGRAM;
-
-    public static PreferredContact fromString(String preference) {
-        for (PreferredContact contact : PreferredContact.values()) {
-            if (contact.name().equalsIgnoreCase(preference)) {
-                return contact;
-            }
-        }
-        throw new IllegalArgumentException("No contact preference with name " + preference + " found");
-    }
 }
