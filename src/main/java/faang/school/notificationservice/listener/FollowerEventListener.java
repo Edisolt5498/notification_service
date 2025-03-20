79c6aae881eb85af94d56c501a2ddb1eb75dcaab
Tatiana Gritsenko <tatyana.gritsenko@roboline.ru>
2025-03-20 17:19:19 +0300

    fix group id

--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -1,9 +1,11 @@
 package faang.school.notificationservice.listener;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FollowerEventListener {
-    @KafkaListener(topics = "follower_topic", groupId = "1")
+    @KafkaListener(topics = "follower_topic", groupId = "user-service-group-ban")
     public void consume(String message) {
         System.out.println("Received message: " + message);
         // Ваша логика обработки сообщения
