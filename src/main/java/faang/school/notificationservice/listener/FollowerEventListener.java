77fff20beba03d127e8ef58f3194be7ea15b5fee
Tatiana Gritsenko <tatyana.gritsenko@roboline.ru>
2025-03-28 17:22:28 +0300

    add email notification

--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -2,12 +2,16 @@ package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserNotificationDto;
 import faang.school.notificationservice.event.FollowerEvent;
+import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +19,10 @@ public class FollowerEventListener {
     private final String followerTopic = "${topic.follower-topic}";
     private final String kafkaConsumerGroupId = "${spring.data.kafka.consumer.group-id}";
 
+
+    private final UserServiceClient userServiceClient;
     private final ObjectMapper objectMapper;
+    private final List<NotificationService> notificationServices;
 
     @KafkaListener(topics = followerTopic, groupId = kafkaConsumerGroupId, properties = {"spring.json.value.default.type=faang.school.notificationservice.event"})
     public void consume(String message) throws IOException {
@@ -26,5 +33,12 @@ public class FollowerEventListener {
 
         System.out.println("Received event: " + event);
         // Ваша логика обработки сообщения
+
+        UserNotificationDto user = userServiceClient.getUserNotificationDto(event.getFolloweeId());
+
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact() == UserNotificationDto.PreferredContact.EMAIL)
+                .findFirst()
+                .ifPresent(service -> service.send(user, "You've got a new follower!"));
     }
 }
